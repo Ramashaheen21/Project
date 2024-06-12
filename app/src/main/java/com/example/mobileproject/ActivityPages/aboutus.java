a082bb0f45860f861f2baa7738a094c2a47d44ae
Ramashaheen21 <Shaheenrama10@gmail.com>
2024-06-12 21:50:26 +0300

    all

--- app/src/main/java/com/example/mobileproject/ActivityPages/aboutus.java
+++ app/src/main/java/com/example/mobileproject/ActivityPages/aboutus.java
@@ -1,4 +1,17 @@
 package com.example.mobileproject.ActivityPages;
 
-public class aboutus {
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mobileproject.R;
+
+public class aboutus extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_about_us);
+    }
 }
